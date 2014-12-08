540310a5d8e24976e0f7b3019d3ad3fbb55f35cc
Dennis Hoer <dennis.hoer@pearson.com>
2014-12-08 01:35:45 -0700

    Fix usleep not found

--- recipes/__libjxgrabkey.rb
+++ recipes/__libjxgrabkey.rb
@@ -13,6 +13,11 @@ end
 
 execute "chmod -R 755 #{home}/jxgrabkey-0.3.2 && chown -R root:root #{home}/jxgrabkey-0.3.2"
 
+# Ubuntu 14.x usleep not found: include unistd.h
+execute "echo '#include <unistd.h>'|cat - JXGrabKey.cpp > /tmp/out && mv /tmp/out JXGrabKey.cpp" do
+  cwd "#{home}/jxgrabkey-0.3.2/JXGrabKey/C++/src"
+end
+
 execute 'chmod +x Makefile && make' do
   cwd "#{home}/jxgrabkey-0.3.2/JXGrabKey/C++"
 end
